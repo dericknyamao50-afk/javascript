6fc7979f29ef9217210cfa6e8decc90d99a9c15e
Derick <dericknyamao50@gmail.com>
2026-06-25 07:58:03 +0300

    javascript

--- ifesleiFelse.js
+++ ifesleiFelse.js
@@ -8,7 +8,12 @@ if (age>18) {
     text="You can drink";
     
 }
-
+if (age<18) {
+    
+} else {
+    console.log("You cannot drink");
+    
+}
 
 console.log("it runs");
 
@@ -17,17 +22,41 @@ if (time<12) {
    console.log("goodmorning");
     
 }
+if (time>12) {
+    
+} else {
+    console.log("good afternoon");
+    
+}
 
 let isstudent=true
 if (isstudent) {
     console.log("you are a student");
     
 }
+else{
+    console.log("please register");
+    
+}
+
+
+    
+
 let activist=18
 if (activist) {
    console.log("you are a protester");
     
 }
+else if (activist<18) {
+    console.log("Stay at home");
+    
+} else {
+    
+}
+
+
+
+
 let name=true
 if (name) {
     console.log("Deroh");
